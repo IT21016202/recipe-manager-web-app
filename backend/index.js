efd2d5802f1213cd60f137e06cd6ef1fdb7b1e55
IT21016202 <it21016202@my.sliit.lk>
2023-11-07 12:14:22 +0530

    backend server and db connection implemented

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// Creating backend server
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors  = require('cors');
+require("dotenv").config(); 
+
+// Importing environment variables
+const port = process.env.PORT || 8080
+const mongo_url = process.env.MONGO_URI;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connecting to MongoDB
+mongoose.connect(mongo_url, {});
+const connection = mongoose.connection; 
+
+connection.once("open", () => {
+  console.log("Database Connection Successful");
+})
+
+// Starting server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
